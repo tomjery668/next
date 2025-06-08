eac8b35b57d809e77b970b6f799267b7fddcf52f
tomjery668 <snprmjr@gmail.com>
2025-06-08 18:07:52 +0545

    first

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
       <Image
         className="cursor-pointer w-28 md:w-32"
-        style={{ width: '95px' }}
+        style={{ width: '94px' }}
         onClick={() => router.push('/')}
         src={assets.logo}
         alt="logo"
